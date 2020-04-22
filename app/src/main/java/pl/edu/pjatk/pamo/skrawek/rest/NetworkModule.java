8cb6221023853561884173d666e1dd3af1aafe42
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-04-23 00:47:32 +0200

    Disable SSL certificate validation
    This is temporary workaround

--- app/src/main/java/pl/edu/pjatk/pamo/skrawek/rest/NetworkModule.java
+++ app/src/main/java/pl/edu/pjatk/pamo/skrawek/rest/NetworkModule.java
@@ -2,6 +2,7 @@ package pl.edu.pjatk.pamo.skrawek.rest;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,12 +10,14 @@ import static pl.edu.pjatk.pamo.skrawek.rest.RequestMappings.BASE_URL;
 
 @Module
 public class NetworkModule {
+    OkHttpClient okHttpClient = UnsafeOkHttpClient.getUnsafeOkHttpClient();
 
     @Provides
     public Retrofit getRetrofit() {
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
     }
 }
