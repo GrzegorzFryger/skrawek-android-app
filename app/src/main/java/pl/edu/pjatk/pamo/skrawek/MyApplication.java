920382f386b009c60d3d9510a7f32b8edc7852d4
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-04-22 23:33:19 +0200

    Add MyApplication class
    This class should allow to access resources strings in whole app

---
+++ app/src/main/java/pl/edu/pjatk/pamo/skrawek/MyApplication.java
@@ -0,0 +1,21 @@
+package pl.edu.pjatk.pamo.skrawek;
+
+import android.app.Application;
+import android.content.res.Resources;
+
+public class MyApplication extends Application {
+
+    private static MyApplication mInstance;
+    private static Resources res;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        res = getResources();
+    }
+
+    public static String getStringFromRes(int id) {
+        return res.getString(id);
+    }
+}
