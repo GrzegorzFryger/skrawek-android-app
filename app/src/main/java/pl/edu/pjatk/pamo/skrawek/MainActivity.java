2af6fad4d28cc37dd1490faf693499497ab81903
Grzegorz <s12583@pjwstk.edu.pl>
2020-04-24 15:21:58 +0200

    Update main activity
    make status bar transparent, and add fragment to selecting children

--- app/src/main/java/pl/edu/pjatk/pamo/skrawek/MainActivity.java
+++ app/src/main/java/pl/edu/pjatk/pamo/skrawek/MainActivity.java
@@ -1,6 +1,10 @@
 package pl.edu.pjatk.pamo.skrawek;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -29,15 +33,29 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //todo temporary removed
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        // make status-bar transparent
+        this.setTransparentTitleBar(getWindow());
 
         // Temporary code for testing
         ((MyApplication) getApplicationContext()).appComponent.inject(this);
         balanceController.start(UUID.fromString("0560d77d-e0db-4914-ae4a-4f39690ecb2d"));
     }
+
+    protected void setTransparentTitleBar(Window window) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_OVERSCAN);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+
+        // set color icon to dark
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+
 
 }
