c7d5d63421ee07c0c0eed0df691237d59843ea7c
Grzegorz <s12583@pjwstk.edu.pl>
2020-04-24 15:19:45 +0200

    Add children fragment

---
+++ app/src/main/java/pl/edu/pjatk/pamo/skrawek/ui/children/ChildrenSelect.java
@@ -0,0 +1,40 @@
+package pl.edu.pjatk.pamo.skrawek.ui.children;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import pl.edu.pjatk.pamo.skrawek.R;
+
+public class ChildrenSelect extends Fragment {
+
+    private ChildrenSelectViewModel mViewModel;
+    private Button button;
+
+    public static ChildrenSelect newInstance() {
+        return new ChildrenSelect();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.children_select_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ChildrenSelectViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/pl/edu/pjatk/pamo/skrawek/ui/children/ChildrenSelectViewModel.java
@@ -0,0 +1,7 @@
+package pl.edu.pjatk.pamo.skrawek.ui.children;
+
+import androidx.lifecycle.ViewModel;
+
+public class ChildrenSelectViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
