d6a44272bba995acd1d92ac922da60e93ffff290
Grzegorz <s12583@pjwstk.edu.pl>
2020-04-24 15:22:27 +0200

    Add finances fragment

---
+++ app/src/main/java/pl/edu/pjatk/pamo/skrawek/ui/finances/FinancesFragment.java
@@ -0,0 +1,36 @@
+package pl.edu.pjatk.pamo.skrawek.ui.finances;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import pl.edu.pjatk.pamo.skrawek.R;
+
+public class FinancesFragment extends Fragment {
+
+    private FinancesViewModel mViewModel;
+
+    public static FinancesFragment newInstance() {
+        return new FinancesFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.finances_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(FinancesViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/pl/edu/pjatk/pamo/skrawek/ui/finances/FinancesViewModel.java
@@ -0,0 +1,7 @@
+package pl.edu.pjatk.pamo.skrawek.ui.finances;
+
+import androidx.lifecycle.ViewModel;
+
+public class FinancesViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
