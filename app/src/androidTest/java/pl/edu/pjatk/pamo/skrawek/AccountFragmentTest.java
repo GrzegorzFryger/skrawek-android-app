c3b52d3dfe7460d80c515dc4b5361287f1e651f2
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-04-30 03:03:40 +0200

    Fix espresso tests and add new one

--- app/src/androidTest/java/pl/edu/pjatk/pamo/skrawek/AccountFragmentTest.java
+++ app/src/androidTest/java/pl/edu/pjatk/pamo/skrawek/AccountFragmentTest.java
@@ -13,8 +13,12 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.isSelected;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static pl.edu.pjatk.pamo.skrawek.ViewActionUtils.waitFor;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -29,11 +33,19 @@ public class AccountFragmentTest {
 
     @Test
     public void Should_LoadAccountFragment() {
-        // Step 1 - Click account icon
+        // Step 0 - Verify that snackbar appears after log in
+        onView(allOf(withId(R.id.snackbar_text), withText("You have successfully logged in")))
+                .check(matches(isDisplayed()));
+
+        // Step 1 - Wait for Snackbar to disappear
+        // Snackbar is visible for 1500 ms - we give some more time to include disappear animation
+        onView(isRoot()).perform(waitFor(3000L));
+
+        // Step 2 - Click account icon
         onView(withId(R.id.navigation_account)).perform(click());
         onView(withId(R.id.navigation_account)).check(matches(isSelected()));
 
-        // Step 2 - Verify that all necessary labels are visible
+        // Step 3 - Verify that all necessary labels are visible
         onView(withId(R.id.nameHeaderTextView)).check(matches(isDisplayed()));
         onView(withId(R.id.addressHeaderTextView)).check(matches(isDisplayed()));
         onView(withId(R.id.phoneHeaderTextView)).check(matches(isDisplayed()));
