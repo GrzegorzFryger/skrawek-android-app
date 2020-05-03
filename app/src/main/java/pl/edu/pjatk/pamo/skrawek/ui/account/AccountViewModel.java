1bfecf81a26705615fedd6d13c431dbd64e25f21
Grzegorz <s12583@pjwstk.edu.pl>
2020-05-03 20:47:25 +0200

    Update account fragment

--- app/src/main/java/pl/edu/pjatk/pamo/skrawek/ui/account/AccountViewModel.java
+++ app/src/main/java/pl/edu/pjatk/pamo/skrawek/ui/account/AccountViewModel.java
@@ -7,21 +7,28 @@ import androidx.lifecycle.ViewModel;
 
 import javax.inject.Inject;
 
-import pl.edu.pjatk.pamo.skrawek.repository.AccountRepository;
 import pl.edu.pjatk.pamo.skrawek.rest.model.accounts.Account;
+import pl.edu.pjatk.pamo.skrawek.rest.model.accounts.Guardian;
 
 public class AccountViewModel extends ViewModel {
     private String NAME_SURNAME_TEMPLATE = "%s %s";
-    private final AccountRepository accountRepository;
     private MutableLiveData<Account> selectedAccount = new MutableLiveData<>();
 
     @Inject
-    public AccountViewModel(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
+    public AccountViewModel() {
+
     }
 
-    public void initializeAccountData(String email) {
-        selectedAccount = accountRepository.getMutableLiveData(email);
+    public void setGuardian(Guardian guardian) {
+        selectedAccount.setValue(guardian);
+    }
+
+    public LiveData<String> getName() {
+        return Transformations.map(selectedAccount, Account::getName);
+    }
+
+    public LiveData<String> getSurname() {
+        return Transformations.map(selectedAccount, Account::getSurname);
     }
 
     public LiveData<String> getAccountOwnerFullName() {
@@ -46,5 +53,9 @@ public class AccountViewModel extends ViewModel {
 
     public LiveData<String> getStatus() {
         return Transformations.map(selectedAccount, account -> account.getStatus().name());
+    }
+
+    public LiveData<Account> getSelectedAccount() {
+        return selectedAccount;
     }
 }
